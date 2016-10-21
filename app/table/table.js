821bbb36c2b1d457fda74fc8be55b8fb05093a10
Dan Kahle <daniel.kahle@acxiom.com>
2016-10-21 11:21:39 -0400

    added link to main page for table view

--- app/table/table.js
+++ app/table/table.js
@@ -2,7 +2,7 @@ angular.module('table', ['ui.bootstrap', 'ui.router']);
 
 angular.module('table').config(function ($stateProvider) {
 
-    $stateProvider.state('Table', {
+    $stateProvider.state('table', {
         url: '/table',
         templateUrl: 'app/table/controllers/Table/Table.html',
     });
